5b332410dc06b26921dc555eb321fd5b539da71a
neerajmj <nmj41198@gmail.com>
2024-03-25 13:15:25 +0530

    apple-25

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+  <div className='bg-gray-200'>
+    <nav className='bg-gray-500 text-white p-4'>
+      <div className='container mx-auto'>
+        <div className='flex justify-center'>
+          <ul className='flex space-x-4'>
+            <li><a href="#" className=''>Store</a></li>
+            <li><a href="#" className=''>Mac</a></li>
+            <li><a href="#" className=''>ipad</a></li>
+            <li><a href="#" className=''>iphone</a></li>
+            <li><a href="#" className=''>Watch</a></li>
+            <li><a href="#" className=''>Vision</a></li>
+            <li><a href="#" className=''>AirPods</a></li>
+            <li><a href="#" className=''>TV&Home Entertainment</a></li>
+            <li><a href="#" className=''>Accessories</a></li>
+            <li><a href="#" className=''>Support</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+  </div>
+  )
+}
+
+export default App
